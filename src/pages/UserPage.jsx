4ba9bb9317f970fbfe043351f1ccfcea3ca8558b
Salman Awal <salmanawal2012@gmail.com>
2025-05-21 20:35:22 +1000

    sal-dynamic userDetails w/ sort functionality and skeleton loading states

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,95 +1,233 @@
 import { faEthereum } from "@fortawesome/free-brands-svg-icons";
 import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Skeleton from "../components/ui/Skeleton";
 
 export default function UserPage() {
+  const { userId } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({});
+  const [userItems, setUserItems] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(12);
+  const [sort, setSort] = useState("");
+  const [sortedItems, setSortedItems] = useState([]);
+
+  async function fetchUser() {
+    setLoading(true);
+    const { data } = await axios.get(
+      `https://remote-internship-api-production.up.railway.app/user/${userId}`
+    );
+    let userData = data.data;
+    setUser(userData);
+
+    if (userData) {
+      let userItemsData = userData.items;
+      setUserItems(userItemsData);
+    }
+    setLoading(false);
+  }
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 6);
+  };
+
+  function renderItems(arr) {
+    return arr?.slice(0, visibleCount).map((item, index) => (
+      <div className="item-column" key={index}>
+        <Link to={`/item/${item.itemId}`} className="item">
+          <figure className="item__img__wrapper">
+            <img src={item.imageLink} alt={item.title} className="item__img" />
+          </figure>
+          <div className="item__details">
+            <span className="item__details__name">{item.title}</span>
+            <span className="item__details__price">{item.price} ETH</span>
+            <span className="item__details__last-sale">
+              Last sale: {item.lastSale} ETH
+            </span>
+          </div>
+          <div className="item__see-more">
+            <button className="item__see-more__button">See More</button>
+            <div className="item__see-more__icon">
+              <FontAwesomeIcon icon={faShoppingBag} />
+            </div>
+          </div>
+        </Link>
+      </div>
+    ));
+  }
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+    fetchUser();
+  }, [userId]);
+
+  useEffect(() => {
+    let items = [...(userItems || [])];
+    if (sort === "HIGH_TO_LOW") {
+      items.sort((a, b) => b.price - a.price);
+    } else if (sort === "LOW_TO_HIGH") {
+      items.sort((a, b) => a.price - b.price);
+    }
+    setSortedItems(items);
+  }, [sort, userItems]);
 
   return (
     <>
-      <header
-        style={{
-          backgroundImage: `url('https://i.seadn.io/s/raw/files/40c1f630bda7d55d859d9107cc86191f.png?auto=format&dpr=1&w=1920')`,
-        }}
-        id="user-header"
-      ></header>
+      {loading ? (
+        <>
+          <header id="user-header">
+            <Skeleton width="100%" height="100%" />
+          </header>
 
-      <section id="user-info">
-        <div className="row">
-          <div className="user-info__wrapper">
-            <figure className="user-info__img__wrapper">
-              <img
-                src="https://i.seadn.io/s/raw/files/55ada1658290f91266c83f075ea03233.png?auto=format&dpr=1&w=256"
-                alt=""
-                className="user-info__img"
-              />
-            </figure>
-            <h1 className="user-info__name">shilpixels</h1>
-            <div className="user-info__details">
-              <span className="user-info__wallet">
-                <FontAwesomeIcon
-                  icon={faEthereum}
-                  className="user-info__wallet__icon"
-                />
-                <span className="user-info__wallet__data">shilpixels.eth</span>
-              </span>
-              <span className="user-info__year">
-                <span className="user-info__year__data">
-                  Joined Feburary 2021
-                </span>
-              </span>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section id="user-items">
-        <div className="row user-items__row">
-          <div className="user-items__header">
-            <div className="user-items__header__left">
-              <span className="user-items__header__text">163 items</span>
-            </div>
-            <select className="user-items__header__sort">
-              <option value="">Recently purchased</option>
-              <option value="">Price high to low</option>
-              <option value="">Price low to high</option>
-            </select>
-          </div>
-          <div className="user-items__body">
-            {new Array(10).fill(0).map((_, index) => (
-              <div className="item-column" key={index}>
-                <Link to={"/item"} className="item">
-                  <figure className="item__img__wrapper">
-                    <img
-                      src="https://i.seadn.io/gcs/files/0a085499e0f3800321618af356c5d36b.png?auto=format&dpr=1&w=384"
-                      alt=""
-                      className="item__img"
-                    />
-                  </figure>
-                  <div className="item__details">
-                    <span className="item__details__name">Meebit #0001</span>
-                    <span className="item__details__price">0.98 ETH</span>
-                    <span className="item__details__last-sale">
-                      Last sale: 7.45 ETH
-                    </span>
-                  </div>
-                  <a className="item__see-more" href="#">
-                    <button className="item__see-more__button">See More</button>
-                    <div className="item__see-more__icon">
-                      <FontAwesomeIcon icon={faShoppingBag} />
-                    </div>
-                  </a>
-                </Link>
+          <section id="user-info">
+            <div className="row">
+              <div className="user-info__wrapper">
+                <figure className="user-info__img__wrapper">
+                  <Skeleton width="100%" height="100%" />
+                </figure>
+                <h1 className="user-info__name">
+                  {" "}
+                  <Skeleton width="250px" height="15px" borderRadius="4px" />
+                </h1>
+                <div className="user-info__details">
+                  <Skeleton width="300px" height="15px" borderRadius="4px" />
+                  <Skeleton width="100px" height="15px" borderRadius="4px" />
+                </div>
               </div>
-            ))}
-          </div>
-        </div>
-        <button className="collection-page__button">Load more</button>
-      </section>
+            </div>
+          </section>
+
+          <section id="user-items">
+            <div className="row user-items__row">
+              <div className="user-items__header">
+                <div className="user-items__header__left">
+                  <span className="user-items__header__text">
+                    <Skeleton width="125px" height="15px" borderRadius="4px" />
+                  </span>
+                </div>
+                <select className="user-items__header__sort">
+                  <option value="">Recently purchased</option>
+                  <option value="">Price high to low</option>
+                  <option value="">Price low to high</option>
+                </select>
+              </div>
+              <div className="user-items__body">
+                {new Array(10).fill(0).map((_, index) => (
+                  <div className="item-column" key={index}>
+                    <div className="item">
+                      <figure className="item__img__wrapper">
+                        <Skeleton
+                          width="100%"
+                          height="100%"
+                          borderRadius="4px"
+                        />
+                      </figure>
+                      <div className="item__details">
+                        <span className="item__details__name">
+                          <Skeleton
+                            width="75px"
+                            height="15px"
+                            borderRadius="4px"
+                          />
+                        </span>
+                        <span className="item__details__price">
+                          <Skeleton
+                            width="50px"
+                            height="15px"
+                            borderRadius="4px"
+                          />
+                        </span>
+                        <span className="item__details__last-sale">
+                          <Skeleton
+                            width="100px"
+                            height="15px"
+                            borderRadius="4px"
+                          />
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        </>
+      ) : (
+        <>
+          <header
+            style={{
+              backgroundImage: `url('${user.imageLink}')`,
+            }}
+            id="user-header"
+          ></header>
+
+          <section id="user-info">
+            <div className="row">
+              <div className="user-info__wrapper">
+                <figure className="user-info__img__wrapper">
+                  <img
+                    src={user.profilePicture}
+                    alt={user.name}
+                    className="user-info__img"
+                  />
+                </figure>
+                <h1 className="user-info__name">{user.name}</h1>
+                <div className="user-info__details">
+                  <span className="user-info__wallet">
+                    <FontAwesomeIcon
+                      icon={faEthereum}
+                      className="user-info__wallet__icon"
+                    />
+                    <span className="user-info__wallet__data">
+                      {user.walletCode}
+                    </span>
+                  </span>
+                  <span className="user-info__year">
+                    <span className="user-info__year__data">
+                      Joined {user.creationDate}
+                    </span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="user-items">
+            <div className="row user-items__row">
+              <div className="user-items__header">
+                <div className="user-items__header__left">
+                  <span className="user-items__header__text">
+                    {userItems.length} items
+                  </span>
+                </div>
+                <select
+                  className="user-items__header__sort"
+                  value={sort}
+                  onChange={(event) => setSort(event.target.value)}
+                >
+                  <option value="" default>
+                    Recently purchased
+                  </option>
+                  <option value="HIGH_TO_LOW">Price high to low</option>
+                  <option value="LOW_TO_HIGH">Price low to high</option>
+                </select>
+              </div>
+              <div className="user-items__body">{renderItems(sortedItems)}</div>
+            </div>
+            {visibleCount < userItems.length && (
+              <button
+                className="collection-page__button"
+                onClick={handleLoadMore}
+              >
+                Load more
+              </button>
+            )}
+          </section>
+        </>
+      )}
     </>
   );
 }
